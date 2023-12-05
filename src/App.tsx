64ce7ae3982a7027567aaac22cc30d51cc2f25ee
veilvokay <roman.yakovliev@hotmail.com>
2023-12-05 09:53:03 +0100

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 import Page from './components/layout/Page';
 import './styles/index.sass';
 
 function App() {
   return (
-    <Page />
+    <>
+      <Page />
+      <Analytics />
+    </>
   );
 }
 
